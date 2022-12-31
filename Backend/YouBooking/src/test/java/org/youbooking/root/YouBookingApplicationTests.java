6754b34970fa3d35a5df9cd41d2e5a7d13632fc0
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-12-31 16:08:31 +0100

    Pushed the backend project structure

---
+++ Backend/YouBooking/src/test/java/org/youbooking/root/YouBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.youbooking.root;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
