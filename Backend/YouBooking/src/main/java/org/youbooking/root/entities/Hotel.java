8284b43776362db846bf78701c769131112670bd
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-02 16:15:56 +0100

    Add some modifs

--- Backend/YouBooking/src/main/java/org/youbooking/root/entities/Hotel.java
+++ Backend/YouBooking/src/main/java/org/youbooking/root/entities/Hotel.java
@@ -1,6 +1,7 @@
 package org.youbooking.root.entities;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.youbooking.root.enums.HotelStateEnum;
@@ -14,6 +15,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
@@ -22,38 +24,48 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 
-@Entity @Getter @Setter @ToString
+@Entity @Getter @Setter @ToString @NoArgsConstructor
 @Table(name = "hotel")
-public class Hotel {
+public class Hotel{
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "hotel_seq")
     @SequenceGenerator(name = "hotel_seq")
-    @Column(name = "id", nullable = false)
+    @Column(nullable = false)
     private Long id;
 
     @Column(nullable = false, length = 50)
     private String name;
 
-    @ToString.Exclude
     @OneToMany(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "hotel_id")
+    @ToString.Exclude
     private Set<BedRoom> bedRooms = new LinkedHashSet<>();
 
-    @ToString.Exclude
     @OneToOne(cascade = CascadeType.PERSIST, optional = false)
     @JoinColumn(name = "address_id", nullable = false)
     private Address address;
 
-    @ToString.Exclude
     @OneToMany(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "hotel_id")
-    private Set<Attachment> attachments = new LinkedHashSet<>();
+    @ToString.Exclude
+    private Set<Attachment> attachments ;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private HotelStateEnum status;
 
     @Column(name = "is_approved")
-    private Boolean isApproved;
+    private Boolean isApproved = null;
 
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private AppUser owner;
+
+    public Hotel(String name, Set<BedRoom> bedRooms, Address address, HotelStateEnum status, AppUser owner) {
+        this.name = name;
+        this.bedRooms = bedRooms;
+        this.address = address;
+        this.status = status;
+        this.owner = owner;
+    }
 }
