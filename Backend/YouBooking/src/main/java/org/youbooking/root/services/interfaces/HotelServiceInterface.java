e201cf9fcddef3036122627755e1542418bfc772
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-02 16:07:59 +0100

    Create the service's interface for the HotelService which contains until now one method ( createHotel )

---
+++ Backend/YouBooking/src/main/java/org/youbooking/root/services/interfaces/HotelServiceInterface.java
@@ -0,0 +1,7 @@
+package org.youbooking.root.services.interfaces;
+
+import org.youbooking.root.services.dtos.HotelDto;
+
+public interface HotelServiceInterface {
+    HotelDto createHotel(HotelDto hotel);
+}
