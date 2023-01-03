74038a8b8d1a5cd2bc4644f5b92ae5f809209f49
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-03 23:42:32 +0100

    Create the methods that will serve the rest apis , also refactoring code by using the entity mapper from the modelMapper library

--- Backend/YouBooking/src/main/java/org/youbooking/root/services/implementations/HotelService.java
+++ Backend/YouBooking/src/main/java/org/youbooking/root/services/implementations/HotelService.java
@@ -3,28 +3,43 @@ package org.youbooking.root.services.implementations;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.youbooking.root.entities.Hotel;
+import org.youbooking.root.entities.Role;
+import org.youbooking.root.enums.RoleEnum;
 import org.youbooking.root.repositories.HotelRepository;
+import org.youbooking.root.repositories.RoleRepository;
 import org.youbooking.root.services.dtos.HotelDto;
 import org.youbooking.root.services.interfaces.HotelServiceInterface;
+import org.youbooking.root.utils.EntityMapping;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 
 @Service("hotel-service") @AllArgsConstructor
 public class HotelService implements HotelServiceInterface {
     private final HotelRepository hotelRepository;
+    private final RoleRepository roleRepository;
 
+    public Set<HotelDto> getAllHotels(){
+        List<Hotel> hotels = hotelRepository.findAll();
+        return hotels.stream()
+                .map(EntityMapping::hotelToHotelDTO)
+                .collect(Collectors.toSet());
+    }
+    public HotelDto getHotel(Long id){
+        Hotel hotel= hotelRepository.findById(id).orElseThrow();
+
+        return EntityMapping.hotelToHotelDTO(hotel);
+    }
     @Override
     public HotelDto createHotel(HotelDto hotel) {
-        Hotel newHotel = new Hotel();
-        newHotel.setId(hotel.getId());
-        newHotel.setName(hotel.getName());
-        newHotel.setAddress(hotel.getAddress());
-        newHotel.setOwner(hotel.getOwner());
-        newHotel.setAttachments(hotel.getAttachments());
-        newHotel.setBedRooms(hotel.getBedRooms());
+        Hotel newHotel = EntityMapping.hotelDTOToHotel(hotel);
 
-        newHotel.setStatus(hotel.getStatus());
+        Role ownerRole = roleRepository.findRoleByName(RoleEnum.HOTEL_OWNER);
 
-        hotelRepository.save(newHotel);
+        newHotel.getOwner().setRole(ownerRole);
 
-        return hotel;
+        return EntityMapping.hotelToHotelDTO(hotelRepository.save(newHotel));
     }
 }
