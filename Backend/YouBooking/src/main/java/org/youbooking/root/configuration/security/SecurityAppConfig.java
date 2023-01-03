58006c35f1206f12560c71f19953e76102bd1095
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-03 23:45:15 +0100

    Create the class that will hold all the security configuration of the application , disable the csrf in order to test my apis in postman

---
+++ Backend/YouBooking/src/main/java/org/youbooking/root/configuration/security/SecurityAppConfig.java
@@ -0,0 +1,17 @@
+package org.youbooking.root.configuration.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityAppConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable();
+    }
+}
