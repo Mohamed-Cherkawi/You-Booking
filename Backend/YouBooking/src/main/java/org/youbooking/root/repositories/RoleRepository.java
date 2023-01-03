314edc4db07759099f4acf0c233752718c26f462
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-03 23:45:46 +0100

    Create the findRoleByName method

--- Backend/YouBooking/src/main/java/org/youbooking/root/repositories/RoleRepository.java
+++ Backend/YouBooking/src/main/java/org/youbooking/root/repositories/RoleRepository.java
@@ -3,6 +3,9 @@ package org.youbooking.root.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.youbooking.root.entities.Role;
+import org.youbooking.root.enums.RoleEnum;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role,Integer> { }
+public interface RoleRepository extends JpaRepository<Role,Integer> {
+    Role findRoleByName(RoleEnum role);
+}
