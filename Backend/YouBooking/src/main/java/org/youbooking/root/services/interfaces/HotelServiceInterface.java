56e763ea05ca8d299eedf1bdee817cb5dced2253
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-04 10:12:08 +0100

    Add the required methods for the service

--- Backend/YouBooking/src/main/java/org/youbooking/root/services/interfaces/HotelServiceInterface.java
+++ Backend/YouBooking/src/main/java/org/youbooking/root/services/interfaces/HotelServiceInterface.java
@@ -2,6 +2,12 @@ package org.youbooking.root.services.interfaces;
 
 import org.youbooking.root.services.dtos.HotelDto;
 
+import java.util.Set;
+
 public interface HotelServiceInterface {
+    Set<HotelDto> getAllHotels();
+    HotelDto getHotel(Long id);
     HotelDto createHotel(HotelDto hotel);
+    HotelDto updateHotel(Long id ,HotelDto hotelDto);
+    void deleteHotel(Long hotelId);
 }
