09687ed3874fdfa331d38e13db3666f19f8ff5b8
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-05 15:20:37 +0100

    Create the BedRoomService , until now i implemented the updateStatus method

---
+++ Backend/YouBooking/src/main/java/org/youbooking/root/services/implementations/BedRoomService.java
@@ -0,0 +1,30 @@
+package org.youbooking.root.services.implementations;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.youbooking.root.entities.BedRoom;
+import org.youbooking.root.repositories.BedRoomRepository;
+import org.youbooking.root.services.dtos.BedRoomDto;
+import org.youbooking.root.services.interfaces.BedRoomServiceInterface;
+import org.youbooking.root.utils.EntityMapping;
+import org.youbooking.root.utils.StatusMapping;
+
+import java.util.Optional;
+
+@Service("bed-room-service") @RequiredArgsConstructor
+public class BedRoomService implements BedRoomServiceInterface {
+    private final BedRoomRepository bedRoomRepository;
+
+
+    @Override
+    public BedRoomDto updateStatus(StatusMapping<Long> statusMapping) {
+        Optional<BedRoom> bedRoom = bedRoomRepository.findById(statusMapping.getId());
+
+        if( bedRoom.isEmpty() )
+            return null;
+
+        bedRoom.get().setStatus(statusMapping.getStatus());
+
+        return EntityMapping.bedRoomToBedRoomDTO(bedRoomRepository.save(bedRoom.get()));
+    }
+}
