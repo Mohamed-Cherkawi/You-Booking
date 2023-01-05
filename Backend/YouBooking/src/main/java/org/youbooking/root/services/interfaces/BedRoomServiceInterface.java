8653cd0676cf65a4d165aa5d781c8b95c3f6d61c
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-05 15:21:00 +0100

    Create the BedRoomServiceInterface for service

---
+++ Backend/YouBooking/src/main/java/org/youbooking/root/services/interfaces/BedRoomServiceInterface.java
@@ -0,0 +1,8 @@
+package org.youbooking.root.services.interfaces;
+
+import org.youbooking.root.services.dtos.BedRoomDto;
+import org.youbooking.root.utils.StatusMapping;
+
+public interface BedRoomServiceInterface {
+    BedRoomDto updateStatus(StatusMapping<Long> statusMapping);
+}
