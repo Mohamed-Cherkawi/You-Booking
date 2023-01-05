86d3f82f4efc906ea2d158e9241a0e4a88633a30
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-05 11:48:08 +0100

    Updated from javax to jakarta

--- Backend/YouBooking/src/main/java/org/youbooking/root/entities/AppUser.java
+++ Backend/YouBooking/src/main/java/org/youbooking/root/entities/AppUser.java
@@ -7,20 +7,20 @@ import lombok.Setter;
 import lombok.ToString;
 import org.youbooking.root.enums.AvailabilityStateEnum;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Id;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
 import java.util.Set;
 
 
