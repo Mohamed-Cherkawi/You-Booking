7c3991a93ee9c6a759375afb69b081d0019c1ac8
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-05 11:50:29 +0100

    Implementing a securityFilterChain instead of extending WebSecurityConfigurerAdapter which is deprecated

--- Backend/YouBooking/src/main/java/org/youbooking/root/configuration/security/SecurityAppConfig.java
+++ Backend/YouBooking/src/main/java/org/youbooking/root/configuration/security/SecurityAppConfig.java
@@ -1,17 +1,21 @@
 package org.youbooking.root.configuration.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
-public class SecurityAppConfig extends WebSecurityConfigurerAdapter {
+public class SecurityAppConfig {
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable();
+
+        return http.build();
     }
 }
