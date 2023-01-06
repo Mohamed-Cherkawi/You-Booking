7d781e002a279b200b3a095f9986561dd0d3d16e
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-06 22:44:55 +0100

    Refactored code to be semantically more readable

--- Backend/YouBooking/src/main/java/org/youbooking/root/controllers/HotelOwnerController.java
+++ Backend/YouBooking/src/main/java/org/youbooking/root/controllers/HotelOwnerController.java
@@ -37,37 +37,42 @@ public class HotelOwnerController {
     public ResponseEntity<HotelDto> getCreatedHotelByOwnerByIdApi(@PathVariable("hotelId") Long id){
         HotelDto hotel = hotelService.getHotel(id);
 
-        return (hotel == null) ? ResponseEntity.status(HttpStatus.GONE).build() : ResponseEntity.ok(hotel);
+        return (hotel == null)
+                ? ResponseEntity.status(HttpStatus.GONE).build()
+                : ResponseEntity.ok(hotel);
     }
     @GetMapping("/fetching-all")
     public ResponseEntity<Set<HotelDto>> getAllCreatedHotelsByOwnerApi(){
         Set<HotelDto> hotels = hotelService.getAllHotels();
 
-        return (hotels.isEmpty()) ? ResponseEntity.status(HttpStatus.NO_CONTENT).build() : ResponseEntity.ok(hotels);
+        return (hotels.isEmpty())
+                ? ResponseEntity.status(HttpStatus.NO_CONTENT).build()
+                : ResponseEntity.ok(hotels);
     }
     @PostMapping("/adding-new")
     public ResponseEntity<HotelDto> addHotelCreatedByOwnerApi(@RequestBody HotelDto hotel){
         return ResponseEntity.ok(hotelService.createHotel(hotel));
     }
     @PutMapping("/updating")
-    public ResponseEntity<Object> updateHotelCreatedByOwnerByIdApi(@RequestBody HotelDto hotelDto){
+    public ResponseEntity<Object> updateHotelCreatedByOwnerIdApi(@RequestBody HotelDto hotelDto){
         HotelDto hotel = hotelService.updateHotel(hotelDto);
 
-        return (hotel == null) ? ResponseEntity.status(HttpStatus.NOT_FOUND).body("Can't update the hotel with the id : " + hotelDto.getId() + " because it was not found ")
+        return (hotel == null)
+                ? ResponseEntity.status(HttpStatus.NOT_FOUND).body("Can't update the hotel with the id : " + hotelDto.getId() + " because it was not found ")
                 : ResponseEntity.ok(hotel);
     }
     @PatchMapping("/updating-bed-room-status")
-    public ResponseEntity<Object> updateBedRoomAvailabilityStatus(@RequestBody StatusMapping<Long> statusMapping){
+    public ResponseEntity<Object> updateBedRoomAvailabilityStatusApi(@RequestBody StatusMapping<Long> statusMapping){
         BedRoomDto bedRoom = bedRoomService.updateStatus(statusMapping);
 
-        return (bedRoom == null) ? ResponseEntity.status(HttpStatus.NOT_FOUND).body("Can't update the status of the bed room with the id : " + statusMapping.getId() + " because it was not found ")
+        return (bedRoom == null)
+                ? ResponseEntity.status(HttpStatus.NOT_FOUND).body("Can't update the status of the bed room with the id : " + statusMapping.getId() + " because it was not found ")
                 : ResponseEntity.ok(bedRoom);
     }
     @DeleteMapping("/deleting")
     public ResponseEntity<String> deleteHotelCreatedByOwnerApi(@RequestBody IdClassMapper<Long> idClassMapper){
-        if( hotelService.deleteHotel(idClassMapper.getId()) )
-            return ResponseEntity.ok("The Hotel With the id " + idClassMapper.getId() + " was deleted successfully");
-
-        return ResponseEntity.ok("The Hotel With the id " + idClassMapper.getId() + " was not found !");
+            return ( hotelService.deleteHotel(idClassMapper.getId()) )
+                    ? ResponseEntity.ok("The Hotel With the id " + idClassMapper.getId() + " was deleted successfully")
+                    : ResponseEntity.status(HttpStatus.NOT_FOUND).body("The Hotel With the id " + idClassMapper.getId() + " was not found !");
     }
 }
