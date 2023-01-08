e31db29cc0172c80239a55c4144f522c04950bf6
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-08 22:28:48 +0100

    Changed updateStatus method's parameter to accept a specific enumType

--- Backend/YouBooking/src/main/java/org/youbooking/root/services/interfaces/BedRoomServiceInterface.java
+++ Backend/YouBooking/src/main/java/org/youbooking/root/services/interfaces/BedRoomServiceInterface.java
@@ -1,8 +1,9 @@
 package org.youbooking.root.services.interfaces;
 
+import org.youbooking.root.enums.BedRoomStateEnum;
 import org.youbooking.root.services.dtos.BedRoomDto;
 import org.youbooking.root.utils.StatusMapping;
 
 public interface BedRoomServiceInterface {
-    BedRoomDto updateStatus(StatusMapping<Long> statusMapping);
+    BedRoomDto updateStatus(StatusMapping<Long,BedRoomStateEnum> statusMapping);
 }
