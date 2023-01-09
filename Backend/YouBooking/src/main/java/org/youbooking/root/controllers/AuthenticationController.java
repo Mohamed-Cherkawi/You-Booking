789022008171e30ca9df1491b14f5e8658bfcc01
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-09 23:09:16 +0100

    Create the controller to register and authenticate user

---
+++ Backend/YouBooking/src/main/java/org/youbooking/root/controllers/AuthenticationController.java
@@ -0,0 +1,33 @@
+package org.youbooking.root.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.youbooking.root.services.dtos.auth.AuthenticationRequestDto;
+import org.youbooking.root.services.dtos.auth.AuthenticationResponse;
+import org.youbooking.root.services.dtos.auth.RegisterRequestDto;
+import org.youbooking.root.services.implementations.AuthenticationService;
+
+@RestController @RequiredArgsConstructor
+@RequestMapping("/api/authenticate")
+public class AuthenticationController {
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<Object> register(@RequestBody RegisterRequestDto request) {
+        AuthenticationResponse authResponse = service.register(request);
+        return (authResponse == null)
+            ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body("This username is already taken try another one")
+            : ResponseEntity.ok(authResponse);
+    }
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequestDto request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+}
