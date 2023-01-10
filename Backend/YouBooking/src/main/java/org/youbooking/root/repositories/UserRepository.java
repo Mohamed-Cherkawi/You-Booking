f8a98301680ecd4304e9e73d59c8591fd9f9a23a
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-10 12:31:46 +0100

    Create user repo that contains for now only method ( findByUsername)

--- Backend/YouBooking/src/main/java/org/youbooking/root/repositories/UserRepository.java
+++ Backend/YouBooking/src/main/java/org/youbooking/root/repositories/UserRepository.java
@@ -4,5 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.youbooking.root.entities.AppUser;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<AppUser,Long> { }
+public interface UserRepository extends JpaRepository<AppUser,Long> {
+    Optional<AppUser> findByUsername(String username);
+}
