5998bc603d9cf1f206197b6826ce553067ff53fd
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-12 22:25:26 +0100

    Add the routing file

---
+++ Frontend/youbooking/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from "@angular/core";
+import {Routes , RouterModule} from "@angular/router";
+import {AppComponent} from "./app.component";
+import {LoginComponent} from "./modules/login/login.component";
+
+const routes: Routes = [
+  {
+    path: '' ,
+    component : AppComponent ,
+    children : [
+      {
+        path : '' ,
+        component : LoginComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
