888fda735a982d57f05d95cea3ba3f455e4f181d
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-12 22:23:22 +0100

    Create the hotel owner methods apis in the service

--- Frontend/youbooking/src/app/services/hotel-owner.service.ts
+++ Frontend/youbooking/src/app/services/hotel-owner.service.ts
@@ -13,43 +13,41 @@ import {IdClassMapper} from "../utils/id-class-mapper.util";
   providedIn: 'root'
 })
 export class HotelOwnerService {
-  private apiServerUrl: string = environment.apisBaseUrl + "/api/hotel/manager";
+  private restPath: string = environment.apisBaseUrl + "/api/hotel/manager";
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   public getCreatedHotelByOwnerByIdApi(hotelId: number): Observable<Hotel | string> {
-    return this.http.get<Hotel | string>(`${this.apiServerUrl}/fetching/track-id/${hotelId}`);
+    return this.http.get<Hotel | string>(`${this.restPath}/fetching/track-id/${hotelId}`);
   }
-
   public getCreatedHotelOfferByOwnerByIdApi(offerId: number): Observable<HotelOffer | string> {
-    return this.http.get<HotelOffer | string>(`${this.apiServerUrl}/fetching/track-offer-id/${offerId}`)
+    return this.http.get<HotelOffer | string>(`${this.restPath}/fetching/track-offer-id/${offerId}`)
 }
   public getAllCreatedHotelsByOwnerApi(): Observable<Hotel[]> {
-    return this.http.get<Hotel[]>(`${this.apiServerUrl}/fetching-all`);
+    return this.http.get<Hotel[]>(`${this.restPath}/fetching-all`);
   }
   public getAllCreatedHotelOffersByOwnerApi(): Observable<HotelOffer[]> {
-    return this.http.get<HotelOffer[]>(`${this.apiServerUrl}/fetching-all-offers`);
+    return this.http.get<HotelOffer[]>(`${this.restPath}/fetching-all-offers`);
   }
   public addHotelCreatedByOwnerApi(hotel: Hotel): Observable<Hotel> {
-    return this.http.post<Hotel>(`${this.apiServerUrl}/adding-new`,hotel);
+    return this.http.post<Hotel>(`${this.restPath}/adding-new`,hotel);
   }
   public addHotelOfferCreatedByOwnerApi(hotelOffer: HotelOffer): Observable<HotelOffer> {
-    return this.http.post<HotelOffer>(`${this.apiServerUrl}/adding-new-request-offer`,hotelOffer);
+    return this.http.post<HotelOffer>(`${this.restPath}/adding-new-request-offer`,hotelOffer);
   }
   public updateHotelCreatedByOwnerIdApi(hotel: Hotel): Observable<Hotel> {
-    return this.http.put<Hotel>(`${this.apiServerUrl}/updating`,hotel);
+    return this.http.put<Hotel>(`${this.restPath}/updating`,hotel);
   }
   public updateBedRoomAvailabilityStatusApi(statusMapper: StatusMapper<AvailabilityStateEnum>): Observable<StatusMapper<AvailabilityStateEnum> | string> {
-    return this.http.patch<StatusMapper<AvailabilityStateEnum> | string>(`${this.apiServerUrl}/updating-bed-room-status`,statusMapper);
+    return this.http.patch<StatusMapper<AvailabilityStateEnum> | string>(`${this.restPath}/updating-bed-room-status`,statusMapper);
   }
   public updateReservationAcceptanceStatusApi(statusMapper: StatusMapper<AcceptanceStateEnum>): Observable<StatusMapper<AcceptanceStateEnum> | string> {
-    return this.http.patch<StatusMapper<AcceptanceStateEnum> | string>(`${this.apiServerUrl}/updating-reservation-status`,statusMapper);
+    return this.http.patch<StatusMapper<AcceptanceStateEnum> | string>(`${this.restPath}/updating-reservation-status`,statusMapper);
   }
   public deleteHotelCreatedByOwnerApi(idClassMapper: IdClassMapper): Observable<string> {
-  return this.http.request<string>('DELETE', `${this.apiServerUrl}/deleting`,{ body: idClassMapper });
+  return this.http.request<string>('DELETE', `${this.restPath}/deleting`,{ body: idClassMapper });
   }
   public deleteHotelOfferCreatedByOwnerApi(idClassMapper: IdClassMapper): Observable<string> {
-    return this.http.request<string>('DELETE', `${this.apiServerUrl}/deleting-offer`,{ body: idClassMapper });
+    return this.http.request<string>('DELETE', `${this.restPath}/deleting-offer`,{ body: idClassMapper });
   }
 }
