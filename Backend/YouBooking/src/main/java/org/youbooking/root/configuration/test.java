0e145a66874f427a2dc20c8031667603bb0aeb7e
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-12 10:02:23 +0100

    Test file

---
+++ Backend/YouBooking/src/main/java/org/youbooking/root/configuration/test.java
@@ -0,0 +1,11 @@
+package org.youbooking.root.configuration;
+
+
+import org.youbooking.root.enums.AvailabilityStateEnum;
+
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("2023-01-06 09:00:00".compareTo("2023-01-09 12:09:09"));
+    }
+}
