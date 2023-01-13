1355f2642c312f81936297360df0949d169640d4
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-13 22:33:42 +0100

    Add some logic code to handle the authentication process

--- Frontend/youbooking/src/app/modules/login/login.component.ts
+++ Frontend/youbooking/src/app/modules/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, Validators, FormBuilder} from "@angular/forms";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/authentication.service";
+import {AuthenticationRequest} from "../../models/auth/authentication-request.model";
+import {HttpErrorResponse} from "@angular/common/http";
+import {AuthenticationResponse} from "../../models/auth/authentication-response.model";
 
 @Component({
   selector: 'app-login',
@@ -8,30 +13,51 @@ import {FormGroup, Validators, FormBuilder} from "@angular/forms";
 })
 export class LoginComponent implements OnInit {
   hide: boolean = false;
-
-  constructor(private fb: FormBuilder) { }
-
+  credentialsError: boolean = false;
   loginForm: FormGroup = this.fb.group({
     username: ['', [Validators.required, Validators.minLength(5)]],
     password: ['', [Validators.required, Validators.minLength(6)]]
   })
-  ngOnInit(): void {}
 
-  onLogin(){
+  constructor(
+    private fb: FormBuilder ,
+    private router: Router ,
+    private authService: AuthenticationService) { }
+
+
+  public ngOnInit(): void {}
+
+  public onLogin(){
     if (!this.loginForm.valid) {
       return;
     }
     this.handleFormButton();
 
-    console.log(this.loginForm.value.password);
+    this.authService.authenticate(new AuthenticationRequest(this.loginForm.value.username,this.loginForm.value.password))
+      .subscribe(
+       {
+       next:
+         (response: AuthenticationResponse | string) => {
+         this.credentialsError = false;
+         console.log(response);
+         this.redirectToHome();
+         },
+       error:
+         (error : HttpErrorResponse) => {
+           this.credentialsError = true;
+         }
+     });
+  }
+  private redirectToHome(){
+    this.router.navigate(['/register']);
   }
 
-  handleFormButton(): void {
+  private handleFormButton(): void {
     const button = document.querySelector("button");
     button!.disabled = true;
     button!.style.opacity = "0.5";
 
-    setTimeout(function () {
+    setTimeout(function() {
       button!.disabled = false;
       button!.style.opacity = "1";
     }, 1000);
