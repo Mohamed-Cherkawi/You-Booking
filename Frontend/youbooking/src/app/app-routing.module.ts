4be34140aca51171b55549812af5275a172ef772
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-13 12:14:43 +0100

    Add the { login } path for the LoginComponent

--- Frontend/youbooking/src/app/app-routing.module.ts
+++ Frontend/youbooking/src/app/app-routing.module.ts
@@ -1,19 +1,9 @@
 import {NgModule} from "@angular/core";
 import {Routes , RouterModule} from "@angular/router";
-import {AppComponent} from "./app.component";
 import {LoginComponent} from "./modules/login/login.component";
 
 const routes: Routes = [
-  {
-    path: '' ,
-    component : AppComponent ,
-    children : [
-      {
-        path : '' ,
-        component : LoginComponent
-      }
-    ]
-  }
+  { path: 'login' , component: LoginComponent }
 ];
 
 @NgModule({
