dd852f1de1907a8d3d1f7d3eac8fb32d8693565e
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-15 23:16:08 +0100

    Add the a TokenInterceptor for eqch angular equest in the providers array , and imported the MateSelectModule for the role dropdown in the register form

--- Frontend/youbooking/src/app/app.module.ts
+++ Frontend/youbooking/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { LoginComponent } from './modules/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,6 +13,8 @@ import {RouterOutlet} from "@angular/router";
 import {MatIconModule} from "@angular/material/icon";
 import {AppRoutingModule} from "./app-routing.module";
 import { RegisterComponent } from './modules/register/register.component';
+import {TokenInterceptor} from "./requests/token.interceptor";
+import {MatSelectModule} from "@angular/material/select";
 
 @NgModule({
   declarations: [
@@ -20,20 +22,28 @@ import { RegisterComponent } from './modules/register/register.component';
     LoginComponent,
     RegisterComponent
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    MatCardModule,
-    MatInputModule,
-    MatButtonModule,
-    ReactiveFormsModule,
-    RouterOutlet,
-    MatIconModule,
-    AppRoutingModule
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MatCardModule,
+        MatInputModule,
+        MatButtonModule,
+        ReactiveFormsModule,
+        RouterOutlet,
+        MatIconModule,
+        AppRoutingModule,
+        MatSelectModule
+    ],
+  providers: [
+    {
+      // First request interceptor
+      provide: HTTP_INTERCEPTORS, // tells Angular to use the TokenInterceptor class as an HTTP interceptor
+      useClass: TokenInterceptor, // tells Angular that there might be multiple interceptors and this one should be added to the list of interceptors.
+      multi: true
+    }
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
