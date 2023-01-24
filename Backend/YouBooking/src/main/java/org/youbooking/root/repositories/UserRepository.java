d12c4665bb950203e0680cb56f52321529cace98
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-24 09:41:52 +0100

    Add a method in the repo to find a user by its role

--- Backend/YouBooking/src/main/java/org/youbooking/root/repositories/UserRepository.java
+++ Backend/YouBooking/src/main/java/org/youbooking/root/repositories/UserRepository.java
@@ -3,10 +3,12 @@ package org.youbooking.root.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.youbooking.root.entities.AppUser;
+import org.youbooking.root.entities.Role;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<AppUser,Long> {
     Optional<AppUser> findByUsername(String username);
+    Optional<AppUser> findByRole(Role role);
 }
