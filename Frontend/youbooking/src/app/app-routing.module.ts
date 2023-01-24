40dd32780a2d768bb86f4fbeb2b5c55375a71b63
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-24 09:53:39 +0100

    Add  the path for the home page

--- Frontend/youbooking/src/app/app-routing.module.ts
+++ Frontend/youbooking/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import {NgModule} from "@angular/core";
 import {Routes , RouterModule} from "@angular/router";
 import {LoginComponent} from "./modules/login/login.component";
 import {RegisterComponent} from "./modules/register/register.component";
+import {HomeComponent} from "./modules/home/home.component";
+import {TestComponent} from "./modules/test/test.component";
 
 const routes: Routes = [
   { path: 'login' , component: LoginComponent },
-  { path: 'register' , component: RegisterComponent}
+  { path: 'register' , component: RegisterComponent},
+  { path: 'home' , component: HomeComponent}
 ];
 
 @NgModule({
