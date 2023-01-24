ff5752b80ca8822e9e0703c9a424cf38536cb004
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-24 09:30:24 +0100

    Add the filter method for the corsFilter Bean Method to allow incoming requests from the frontend

--- Backend/YouBooking/src/main/java/org/youbooking/root/configuration/security/SecurityConfig.java
+++ Backend/YouBooking/src/main/java/org/youbooking/root/configuration/security/SecurityConfig.java
@@ -9,7 +9,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.youbooking.root.configuration.security.jwt.JwtAuthenticationFilter;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 @Configuration
@@ -34,8 +40,24 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilter(corsFilter());
 
         return http.build();
+    }
+    public CorsFilter corsFilter() {
+        final String ACAO = "Access-Control-Allow-Origin";
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", ACAO, "Content-Type",
+                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+                ACAO, ACAO, "Access-Control-Allow-Credentials"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT","PATCH" ,"DELETE", "OPTIONS"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
     }
 }
