924f2e6b78b2d41591c4b7612b2514182e10655c
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-01-24 09:49:46 +0100

    Add the register component with its html , css and business logic code

--- Frontend/youbooking/src/app/modules/register/register.component.ts
+++ Frontend/youbooking/src/app/modules/register/register.component.ts
@@ -4,6 +4,8 @@ import {Router} from "@angular/router";
 import {AuthenticationService} from "../../services/authentication.service";
 import {RegisterRequest} from "../../models/auth/register-request.model";
 import {Role} from "../../utils/role.util";
+import {AuthenticationResponse} from "../../models/auth/authentication-response.model";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-register',
@@ -14,36 +16,63 @@ export class RegisterComponent implements OnInit {
   hide: boolean = false;
   isUsernameUsed: boolean = false;
   registerRequest: RegisterRequest = new RegisterRequest();
-  registerForm: FormGroup = this.fb.group({
-    username: ['', [Validators.required, Validators.minLength(5)]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-    name: this.fb.group({
-      firstName: ['' , [Validators.required , Validators.minLength(1)]],
-      lastName: ['' , [Validators.required , Validators.minLength(1)]],
-    }) ,
-    phone: ['' , [Validators.required , Validators.minLength(10)]],
-    role: ['' , Validators.required]
-  });
+  registerForm!: FormGroup ;
   roles: Role[] = [
-    {value: 'client', viewValue: 'Client'},
-    {value: 'hotel-owner', viewValue: 'Hotel Owner'}
-  ]
+    {value: 'CLIENT', viewValue: 'Client'},
+    {value: 'OWNER', viewValue: 'Hotel Owner'}
+  ];
+
   constructor(
     private fb: FormBuilder ,
     private router: Router ,
     private authService: AuthenticationService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(5)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      name: this.fb.group({
+        firstName: ['' , [Validators.required , Validators.minLength(1)]],
+        lastName: ['' , [Validators.required , Validators.minLength(1)]],
+      }) ,
+      phone: ['' , [Validators.required , Validators.minLength(10)]],
+      role: ['' , Validators.required]
+    });
+  }
 
   public onRegister() {
+    localStorage.removeItem('token');
+
     if(!this.registerForm.valid)
       return;
 
     this.handleFormButton();
-    console.log(this.registerForm.value)
+    this.registerRequest.setUsername(this.registerForm.value.username);
+    this.registerRequest.setPassword(this.registerForm.value.password);
+    this.registerRequest.setRole(this.registerForm.value.role);
+    this.registerRequest.setPhone(this.registerForm.value.phone);
+    this.registerRequest.setName(
+      this.registerForm.value.name.firstName + ' ' +
+      this.registerForm.value.name.lastName);
 
+    this.authService.register(this.registerRequest)
+      .subscribe(
+        {
+          next:
+            (response: AuthenticationResponse) => {
+              console.log(response);
+              localStorage.setItem('token',response.token);
+              this.redirectToHome();
+            },
+          error:
+            (error : HttpErrorResponse) => {
+              this.isUsernameUsed = true;
+            }
+        });
   }
-
+  private redirectToHome(){
+    this.router.navigate(['/home']);
+  }
   private handleFormButton(): void {
     const button = document.querySelector("button");
     button!.disabled = true;
